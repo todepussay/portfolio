835423901fb9c11f8218160ac4385f82cae1fb3b
Tom <todepussay@stpbb.org>
2023-04-04 16:50:28 +0200

    Javascript age

---
+++ app.js
@@ -0,0 +1,17 @@
+var today = new Date();
+var now = today.toLocaleDateString();
+
+var anniv = "14/07/2003";
+var an = anniv.substr(6,4);
+var mois = anniv.substr(3,2);
+var day = anniv.substr(0,2);
+
+var dateNaissance = new Date(an + "-" + mois + "-" + day);
+
+var age = today.getFullYear() - dateNaissance.getFullYear();
+var m = today.getMonth() - dateNaissance.getMonth();
+if (m < 0 || (m === 0 && today.getDate() < dateNaissance.getDate())) {
+    age--;
+}
+
+document.getElementById('age').innerHTML = age;
